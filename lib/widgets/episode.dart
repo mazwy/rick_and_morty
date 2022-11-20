6448b26d24eb397388316ec1fcc7aba69775450c
mazwy <s26131@pjwstk.edu.pl>
2022-11-20 20:17:20 +0100

    query connected

--- lib/widgets/episode.dart
+++ lib/widgets/episode.dart
@@ -1,43 +1,18 @@
-// import 'package:flutter/material.dart';
-// import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:rick_and_morty/trash/episode_characters.dart';
+import '../main.dart';
 
-// final Link apiLink = HttpLink('https://rickandmortyapi.com/graphql');
+class Episode extends StatelessWidget {
+  const Episode({super.key});
 
-// ValueNotifier<GraphQLClient> client = ValueNotifier(
-//   GraphQLClient(
-//     link: HttpLink('https://rickandmortyapi.com/graphql'),
-//     cache: GraphQLCache(store: HiveStore()),
-//   )
-// );
-
-// class EpisodeWidget extends StatefulWidget {
-//   const EpisodeWidget({super.key});
-
-//   @override
-//   State<EpisodeWidget> createState() => _EpisodeWidgetState();
-// }
-
-// class _EpisodeWidgetState extends State<EpisodeWidget> {
-//   final result = await client.
-
-
-//   final List<String> entries = <String>['A', 'B', 'C'];
-//   final List<int> colorCodes = <int>[600, 500, 100];
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Episode: '),
-//       ),
-//       body: ListView.builder(
-//         padding: const EdgeInsets.all(16.0),
-//         itemBuilder: (context, i) {
-//           return ListTile(
-//             title: Text(entries[i]),
-//           );
-//         },
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar (
+        title: const Text('test')
+      ),
+    );
+  }
+}
