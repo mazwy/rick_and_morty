c1023464cac838c5f307b25b8a4311c8cb47c4ea
mazwy <maczwierzy@gmail.com>
2022-11-27 10:23:25 +0100

    more work on encapsulation and also proper caching

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,7 @@ import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:rick_and_morty/all_episodes/episodes_list.dart';
 import 'package:rick_and_morty/queries.dart';
 
-void main() async {
-  await initHiveForFlutter();
+void main() {
   runApp(const RickAndMortyApp());
 }
 
@@ -15,7 +14,8 @@ class RickAndMortyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     ValueNotifier<GraphQLClient> client = ValueNotifier(
       GraphQLClient(
-        cache: GraphQLCache(store: HiveStore()),
+        cache: GraphQLCache(
+            partialDataPolicy: PartialDataCachePolicy.acceptForOptimisticData),
         link: apiLink,
       ),
     );
